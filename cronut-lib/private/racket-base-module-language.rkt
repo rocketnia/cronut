af053c2e61c91fd79d2d523175452da4700206e1
rocketnia <rokitna@hotmail.com>
2021-08-24 20:05:18 -0700

    Start implementing a partial expander for `#lang cronut/racket/base` module bodies.

--- cronut-lib/private/racket-base-module-language.rkt
+++ cronut-lib/private/racket-base-module-language.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang parendown racket/base
 
 ; cronut/private/racket-base-module-language.rkt
 ;
@@ -23,14 +23,149 @@
 ;   language governing permissions and limitations under the License.
 
 
-(require (for-syntax (only-in syntax/parse expr)))
+(require #/for-syntax racket/base)
 
-(require (only-in syntax/parse/define define-syntax-parse-rule))
+(require #/for-syntax #/only-in cronut/private/shim
+  shim-require-various)
+
+(begin-for-syntax #/shim-require-various)
+
+(require #/only-in cronut/private/shim shim-require-various)
+
+(shim-require-various)
 
 
-(provide (except-out (all-from-out racket/base) #%module-begin))
-(provide (rename-out [-#%module-begin #%module-begin]))
+(provide #/except-out (all-from-out racket/base) #%module-begin)
+(provide
+  ; TODO: See if we should really provide these from here.
+  (for-syntax cronut-declaration-macro?)
+  (for-syntax gen:cronut-declaration-macro)
+  (for-syntax cronut-declaration-macro-call)
+  #%cronut-declaration
+  (for-syntax expand-cronut-module-begin)
+  example-cronut-declaration)
+(provide #/rename-out [-#%module-begin #%module-begin])
 
 
-(define-syntax-parse-rule (-#%module-begin decl:expr ...)
-  (#%module-begin decl ...))
+
+(begin-for-syntax #/define-generics cronut-declaration-macro
+  (cronut-declaration-macro-call cronut-declaration-macro stx decls))
+
+
+(begin-for-syntax #/struct cronut-declaration-representation ()
+  #:property prop:expansion-contexts (list 'module)
+  #:property prop:procedure
+  (fn self stx
+    (error "encountered a Cronut declaration in a non-Cronut Racket module")))
+
+(define-syntax #%cronut-declaration
+  (cronut-declaration-representation))
+
+
+(define-for-syntax (expand-cronut-module-begin decls)
+  (expect decls (cons decl decls) #'(begin)
+  #/syntax-parse
+    (local-expand decl 'module
+      (list
+        
+        ; Syntaxes that expect to be part of the stop list so they can
+        ; cooperate with a module body.
+        ;
+        ; TODO: See which of the other syntaxes we should put here.
+        ;
+        #'define-values
+        #'#%provide
+        
+        #'#%cronut-declaration))
+    [
+      ({~literal #%cronut-declaration} ~! {~and call (op . args)})
+      ; TODO: See if we should add a `disappeared-use` syntax
+      ; property here.
+      (w- op (syntax-local-value #'op)
+      #/expect (cronut-declaration-macro? op) #t
+        (error "encountered a #%cronut-declaration with an operation that wasn't a Cronut declaration macro")
+      #/cronut-declaration-macro-call op #'call decls)]
+    [
+      ({~literal begin} ~! begin-decls:expr ...)
+      (expand-cronut-module-begin
+        (append (syntax->list #'(begin-decls ...)) decls))]
+    [
+      (
+        {~or
+          
+          ; Expression syntaxes that are part of both the
+          ; `local-expand` default stop list and the syntax of
+          ; module-level declarations in fully expanded programs.
+          {~literal begin0}
+          {~literal case-lambda}
+          {~literal if}
+          {~literal letrec-values}
+          {~literal let-values}
+          {~literal #%plain-app}
+          {~literal #%plain-lambda}
+          {~literal set!}
+          {~literal #%top}
+          {~literal #%variable-reference}
+          {~literal with-continuation-mark}
+          
+          ; Expression syntaxes that are part of the `local-expand`
+          ; default stop list but not part of the syntax of
+          ; module-level declarations in fully expanded programs.
+          {~literal #%expression}
+          {~literal letrec-syntaxes+values}
+          
+          ; Expression syntaxes that are part of the syntax of
+          ; module-level declarations in fully expanded programs but
+          ; not part of the `local-expand` default stop list.
+          {~literal quote}
+          {~literal quote-syntax}
+          
+          ; Non-expression syntaxes that are part of the syntax of
+          ; module-level declarations in fully expanded programs. Note
+          ; that these are  not part of the `local-expand` default
+          ; stop list.
+          ;
+          ; TODO: We should probably add several of these to the stop
+          ; list to prevent recursive expansion of their subforms, and
+          ; we should probably handle them with custom behavior.
+          ;
+          {~literal begin-for-syntax}
+          {~literal define-syntaxes}
+          {~literal define-values}
+          {~literal #%declare}
+          {~literal module}
+          {~literal module*}
+          {~literal #%provide}
+          {~literal #%require}
+          
+          }
+        . args)
+      #`(begin #,decl #,(expand-cronut-module-begin decls))]
+    [_ (error "Cronut internal error: unexpected local-expand result")]))
+
+(begin-for-syntax #/struct module-begin-representation ()
+  #:property prop:expansion-contexts (list 'module-begin)
+  #:property prop:procedure
+  (fn self stx
+    (syntax-protect
+    #/syntax-parse stx #/ (_ decl:expr ...)
+    #/w- decl (expand-cronut-module-begin (syntax->list #'(decl ...)))
+      #`(#%module-begin #,decl))))
+
+(define-syntax -#%module-begin (module-begin-representation))
+
+
+(begin-for-syntax #/struct example-cronut-declaration-representation
+  ()
+  #:property prop:expansion-contexts (list 'module)
+  #:property prop:procedure
+  (fn self stx
+    (syntax-protect
+      #`(#%cronut-declaration #,stx)))
+  #:methods gen:cronut-declaration-macro
+  [
+    (define (cronut-declaration-macro-call self stx decls)
+      #`(begin stx #,(expand-cronut-module-begin decls)))])
+
+(define-syntax example-cronut-declaration
+  (example-cronut-declaration-representation))
